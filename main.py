ad675a01b64efb5ee23b76abc42c9a673ae67e4a
bsjohnson20 <bsjohnson2004@gmail.com>
2025-02-16 14:07:32 +0000

    fix time error

--- main.py
+++ main.py
@@ -15,14 +15,14 @@ from functools import lru_cache
 
 
 
-from time import time
+from time import time as now
 def timer_func(func): 
     # This function shows the execution time of  
     # the function object passed 
     def wrap_func(*args, **kwargs): 
-        t1 = time() 
+        t1 = now() 
         result = func(*args, **kwargs) 
-        t2 = time() 
+        t2 = now() 
         time_str = (f'Function {func.__name__!r} executed in {(t2-t1):.4f}s') 
         with open('timings.txt', 'a') as f:
             f.write(time_str + '\n')
